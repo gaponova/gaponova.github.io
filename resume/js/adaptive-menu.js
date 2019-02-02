56b4b198092e411571f168aa437a721e08d74fa8
gaponova <dzensmart@gmail.com>
2019-02-02 21:26:00 +0200

    Загрузили проект resume

---
+++ resume/js/adaptive-menu.js
@@ -0,0 +1,10 @@
+$(function() {
+    
+   $('.menuToggle').on('click', function(){
+			$('.main-nav').slideToggle(500, function(){
+				if($(this).css('display') === 'none'){
+					$(this).removeAttr('style');
+				}
+			});
+		});
+});
